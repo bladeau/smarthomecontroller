fb0877f2da3438c1f6fbe2592924ace657956c80
bladeau <al.almaktoum@live.com>
2020-12-10 16:13:21 +1100

    n/a

--- src/App.tsx
+++ src/App.tsx
@@ -34,13 +34,6 @@ import '@ionic/react/css/display.css'
 /* Theme variables */
 import './theme/variables.css'
 
-interface pairedList {
-  class: number
-  id: string
-  address: string
-  name: string
-}
-
 const App: React.FC = () => {
   return (
     <IonApp>
